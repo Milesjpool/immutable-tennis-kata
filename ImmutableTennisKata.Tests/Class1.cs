6d65f4fd18ad6167907217fb6e274d7128e0f4e4
MilesJPool <MilesJPool@gmail.com>
2016-07-28 11:30:44 +0100

    Allow player one to score

--- ImmutableTennisKata.Tests/Class1.cs
+++ ImmutableTennisKata.Tests/Class1.cs
@@ -3,22 +3,48 @@
 namespace ImmutableTennisKata.Tests
 {
 	[TestFixture]
-    public class GameTests
-    {
+	public class NewGameTests
+	{
 		[Test]
-		public void AGameStartsAtLoveLove()
+		public void GameStartsAtLoveLove()
 		{
 			var game = new Game();
 
 			Assert.That(game.Score, Is.EqualTo("Love-Love"));
 		}
-    }
+	}
+
+	[TestFixture]
+	public class PlayerOneScoresTests
+	{
+		[Test]
+		public void ScoreIsThen15Love()
+		{
+			var game = new Game().PlayerOneScores();
+
+			Assert.That(game.Score, Is.EqualTo("15-Love"));
+		}
+	}
+
 
 	public class Game
 	{
+		private string _score;
+
+		public Game()
+		{
+			_score = "Love-Love";
+		}
+
 		public string Score
 		{
-			get { return "Love-Love"; }
+			get { return _score; }
+		}
+
+		public Game PlayerOneScores()
+		{
+			_score = "15-Love";
+			return this;
 		}
 	}
 }
