b06a35a25a6f6886ad0578db19d4d8abae339ce1
MilesJPool <MilesJPool@gmail.com>
2016-07-28 11:16:37 +0100

    Add Nunit

--- ImmutableTennisKata.Tests/Class1.cs
+++ ImmutableTennisKata.Tests/Class1.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
 
 namespace ImmutableTennisKata.Tests
 {
+	[TestFixture]
     public class Class1
     {
+		[Test]
+		public void Test_name()
+		{
+			
+		}
     }
 }
